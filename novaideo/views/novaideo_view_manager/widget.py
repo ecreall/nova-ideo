ff36e90de2facb3fd5d78350362870a5d3426f34
amen souissi <amensouissi@ecreall.com>
2014-08-09 01:50:36 +0200

    add search form widget

---
+++ novaideo/views/novaideo_view_manager/widget.py
@@ -0,0 +1,11 @@
+import deform
+
+
+
+class SearchFormWidget(deform.widget.FormWidget):
+    template = 'novaideo:views/novaideo_view_manager/templates/search_form.pt'
+
+
+class SearchTextInputWidget(deform.widget.TextInputWidget):
+    template = 'novaideo:views/novaideo_view_manager/templates/textinput.pt'
+
