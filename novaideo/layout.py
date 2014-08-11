9590dda32f8ded573df24ec7bcbd1fcbd468027f
Vincent Fretin <vincent.fretin@gmail.com>
2014-08-11 13:59:06 +0200

    add layout.to_localized_time

--- novaideo/layout.py
+++ novaideo/layout.py
@@ -7,3 +7,9 @@ class GlobalLayout(object):
     def __init__(self, context, request):
         self.context = context
         self.request = request
+
+    def to_localized_time(self, date, date_only=False):
+        if date_only:
+            return date.strftime('%d/%m/%Y')
+        else:
+            return date.strftime('%d/%m/%Y %H:%M')
