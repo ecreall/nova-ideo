712425948e8660f00e805d1a484e3140cda2284c
Vincent Fretin <vincent.fretin@gmail.com>
2014-08-11 21:38:47 +0200

    add infinite scroll to #results

--- novaideo/static/js/novaideo.js
+++ novaideo/static/js/novaideo.js
@@ -10,6 +10,26 @@ $(document).ready(function(){
   $('.panel-collapse').on('show.bs.collapse', function () {
     $(this).siblings().find('a span').attr('class', 'glyphicon glyphicon-minus');
   });
+  $('#results').infinitescroll({
+    navSelector  : "#results .batch",
+                   // selector for the paged navigation (it will be hidden)
+    nextSelector : "#results .pager .next",
+                   // selector for the NEXT link (to page 2)
+    itemSelector : "#results div.col-md-12",
+                   // selector for all items you'll retrieve
+    pathParse: function(path, next_page) {
+       var parts = path.match(/^(.*?batch_num=)1(.*|$)/).slice(1);
+// ["http://0.0.0.0:6543/@@seemyideas?batch_num=", "&batch_size=1&action_uid=-4657697968224339750"]
+// substanced Batch starts at 0, not 1
+       var f = function(currPage) {
+         return parts.join(currPage - 1);
+       };
+       return f;
+    },
+    loading: {
+      finishedMsg: "<em>No more item.</em>"
+    }
+  });
 });
 
 (function () { 
