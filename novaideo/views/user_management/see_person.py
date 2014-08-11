0c8b0ebac86acace79a5fed555b2f9161d4bbb1d
Vincent Fretin <vincent.fretin@gmail.com>
2014-08-11 14:07:21 +0200

    pep8

--- novaideo/views/user_management/see_person.py
+++ novaideo/views/user_management/see_person.py
@@ -1,17 +1,8 @@
-import re
-import colander
 from pyramid.view import view_config
-from pyramid.threadlocal import get_current_registry
 
-from dace.util import find_catalog
 from dace.processinstance.core import DEFAULTMAPPING_ACTIONS_VIEWS
-from dace.util import getSite, allSubobjectsOfType
 from dace.objectofcollaboration.principal.util import get_current
-from pontus.view import BasicView, ViewError, merge_dicts
-from pontus.dace_ui_extension.interfaces import IDaceUIAPI
-from pontus.widget import CheckboxChoiceWidget, RichTextWidget
-from pontus.schema import Schema
-from pontus.form import FormView
+from pontus.view import BasicView, merge_dicts
 
 from novaideo.content.processes.user_management.behaviors import  SeePerson
 from novaideo.content.person import Person
@@ -30,12 +21,12 @@ class SeePersonView(BasicView):
     behaviors = [SeePerson]
     template = 'novaideo:views/user_management/templates/see_person.pt'
     viewid = 'seeperson'
-    requirements = {'css_links':[],
-                    'js_links':['novaideo:static/js/novaideo.js']}
+    requirements = {'css_links': [],
+                    'js_links': ['novaideo:static/js/novaideo.js']}
 
 
     def update(self):
-        self.execute(None) 
+        self.execute(None)
         user = self.context
         ideas = [o for o in getattr(user, 'ideas', []) if o.actions]
         len_result = len(ideas)
@@ -43,25 +34,25 @@ class SeePersonView(BasicView):
         result = {}
         current_user = get_current()
         for o in ideas:
-            object_values = {'object':o, 'current_user':current_user}
-            body = self.content(result=object_values, template=o.result_template)['body']
+            object_values = {'object': o, 'current_user': current_user}
+            body = self.content(result=object_values,
+                    template=o.result_template)['body']
             result_body.append(body)
-        values = {
-                'bodies': result_body,
-                'length': len_result
-               }
-        ideas_body = self.content(result=values, template=SearchResultView.template)['body']
+        values = {'bodies': result_body,
+                  'length': len_result,
+                  }
+        ideas_body = self.content(result=values,
+                template=SearchResultView.template)['body']
 
-        #TOTDO proposals...
+        #TODO proposals...
         values = {'ideas': (result_body and ideas_body) or None,
-                  'proposals':None,
+                  'proposals': None,
                   'user': self.context}
         body = self.content(result=values, template=self.template)['body']
         item = self.adapt_item(body, self.viewid)
-        result['coordinates'] = {self.coordinates:[item]}
-        result  = merge_dicts(self.requirements_copy, result)
+        result['coordinates'] = {self.coordinates: [item]}
+        result = merge_dicts(self.requirements_copy, result)
         return result
 
 
 DEFAULTMAPPING_ACTIONS_VIEWS.update({SeePerson:SeePersonView})
-
