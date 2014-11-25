37a10596d1448f148682f522fbcb9255657d633d
amen souissi <amensouissi@ecreall.com>
2014-11-25 16:59:34 +0100

    add user management actions

---
+++ novaideo/views/user_management/activate.py
@@ -0,0 +1,30 @@
+
+from pyramid.view import view_config
+
+from dace.processinstance.core import DEFAULTMAPPING_ACTIONS_VIEWS
+from pontus.view import BasicView
+
+from novaideo.content.processes.user_management.behaviors import  Activate
+from novaideo.content.person import Person
+from novaideo import _
+
+
+@view_config(
+    name='activate',
+    context=Person,
+    renderer='pontus:templates/view.pt',
+    )
+class ActivateView(BasicView):
+    title = _('Activate the member')
+    name = 'activate'
+    behaviors = [Activate]
+    viewid = 'activateview'
+
+
+    def update(self):
+        self.execute(None)        
+        return list(self.behaviorinstances.values())[0].redirect(
+                                       self.context, self.request)
+
+
+DEFAULTMAPPING_ACTIONS_VIEWS.update({Activate:ActivateView})
---
+++ novaideo/views/user_management/deactivate.py
@@ -0,0 +1,31 @@
+
+from pyramid.view import view_config
+
+from dace.processinstance.core import DEFAULTMAPPING_ACTIONS_VIEWS
+from pontus.view import BasicView
+
+
+from novaideo.content.processes.user_management.behaviors import  Deactivate
+from novaideo.content.person import Person
+from novaideo import _
+
+
+@view_config(
+    name='deactivate',
+    context=Person,
+    renderer='pontus:templates/view.pt',
+    )
+class DeactivateView(BasicView):
+    title = _('Deactivate the member')
+    name = 'deactivate'
+    behaviors = [Deactivate]
+    viewid = 'deactivateview'
+
+
+    def update(self):
+        self.execute(None)        
+        return list(self.behaviorinstances.values())[0].redirect(
+                                       self.context, self.request)
+
+
+DEFAULTMAPPING_ACTIONS_VIEWS.update({Deactivate:DeactivateView})
