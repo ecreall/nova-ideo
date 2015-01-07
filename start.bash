d177051b8ed2d90b64b6055ef3c40f9a5ab7f0f5
Vincent Fretin <vincent.fretin@gmail.com>
2015-01-07 20:43:40 +0100

    add specific production-heroku.ini with placeholders that are filled when the container starts

--- start.bash
+++ start.bash
@@ -1,3 +1,11 @@
 #!/bin/bash
 sed -i 's@/tmp/build@/app@' develop-eggs/* bin/*
-exec bin/gunicorn --forwarded-allow-ips="172.17.42.1" --paste production.ini
+SECRET="${SECRET:-dreevTatUk9}"
+SENDER="${SENDER:-site@example.com}"
+APPLICATION_URL="${APPLICATION_URL:-novaideo-applicationurl}"
+sed -i \
+    -e "s/SECRET/$SECRET/" \
+    -e "s/SENDER/$SENDER/" \
+    -e "s/APPLICATION_URL/$APPLICATION_URL/" \
+    production-heroku.ini
+exec bin/gunicorn --forwarded-allow-ips="172.17.42.1" --paste production-heroku.ini
