25c74f9ac3d1490b798fe5f142eab0641345d57d
amen souissi <amensouissi@ecreall.com>
2015-01-09 15:19:41 +0100

    add license

--- novaideo/event.py
+++ novaideo/event.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2015 by Ecreall under licence AGPL terms 
+# avalaible on http://www.gnu.org/licenses/agpl.html 
+
+# licence: AGPL
+# author: Amen Souissi
 
 from zope.interface import implementer
 from zope.interface.interfaces import IObjectEvent
