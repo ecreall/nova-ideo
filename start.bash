40fb935d47015f0179a608b9cc3b91fc6097bd52
Vincent Fretin <vincent.fretin@gmail.com>
2015-01-25 23:23:58 +0100

    create var/filestorage var/blobstorage before starting up the application

--- start.bash
+++ start.bash
@@ -12,4 +12,5 @@ sed -i \
     -e "s|SECRET|$SECRET|" \
     -e "s|APPLICATION_URL|$APPLICATION_URL|" \
     production-heroku.ini
+mkdir -p var/filestorage var/blobstorage
 exec bin/gunicorn --forwarded-allow-ips="172.17.42.1" --paste production-heroku.ini
