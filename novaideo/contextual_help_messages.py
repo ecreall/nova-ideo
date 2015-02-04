5dc9379c90d5164b0372cc7b821fd56f6774a546
amen souissi <amensouissi@ecreall.com>
2015-02-04 14:15:02 +0100

    add contextual help

---
+++ novaideo/contextual_help_messages.py
@@ -0,0 +1,96 @@
+
+from dace.objectofcollaboration.principal.util import Anonymous, has_role
+
+from novaideo.content.novaideo_application import NovaIdeoApplication
+from novaideo.content.proposal import Proposal
+from novaideo.content.person import Person
+from novaideo.content.idea import Idea
+
+
+def homepage_condition(context, user):
+    return isinstance(user, Anonymous) 
+
+
+def homepage_connected_condition(context, user):
+    return not isinstance(user, Anonymous) 
+
+
+def idea_submited_owner(context, user):
+    return has_role(role=('Owner', context))
+
+
+def idea_submited_moderator(context, user):
+    return has_role(role=('Moderator', ))
+
+
+CONTEXTUAL_HELP_MESSAGES = {
+	(NovaIdeoApplication, 'any', ''): [
+	   (homepage_condition, 'novaideo:views/templates/panels/'
+	   	                    'contextual_help_messages/homepage_message.pt', 1),
+	   (homepage_connected_condition, 'novaideo:views/templates/panels/'
+	   	           'contextual_help_messages/homepage_connected_message.pt', 1)],
+
+	(NovaIdeoApplication, 'any', 'seemycontents'): [
+	   (None, 'novaideo:views/templates/panels/'
+	   	                    'contextual_help_messages/see_my.pt', 1)],
+
+	(NovaIdeoApplication, 'any', 'seemyparticipations'): [
+	   (None, 'novaideo:views/templates/panels/'
+	   	                    'contextual_help_messages/see_my.pt', 1)],
+
+	(NovaIdeoApplication, 'any', 'seemyselections'): [
+	   (None, 'novaideo:views/templates/panels/'
+	   	                    'contextual_help_messages/see_my.pt', 1)],
+
+	(NovaIdeoApplication, 'any', 'seemysupports'): [
+	   (None, 'novaideo:views/templates/panels/'
+	   	                    'contextual_help_messages/see_my.pt', 1)],
+
+	(NovaIdeoApplication, 'any', 'createidea'): [
+	   (None, 'novaideo:views/templates/panels/'
+	   	                    'contextual_help_messages/create_idea.pt', 1)],
+
+	(Proposal, 'draft', 'index'): [
+	   (None, 'novaideo:views/templates/panels/'
+	   	                    'contextual_help_messages/proposal_draft.pt', 1)],
+
+	(Proposal, 'any', 'editproposal'): [
+	   (None, 'novaideo:views/templates/panels/'
+	   	                    'contextual_help_messages/edit_proposal.pt', 1)],
+
+	(Proposal, 'open to a working group', 'index'): [
+	   (None, 'novaideo:views/templates/panels/'
+	   	      'contextual_help_messages/proposal_open_to_a_working_group.pt', 1)],
+
+	(Person, 'any', 'index'): [
+	   (None, 'novaideo:views/templates/panels/'
+	   	                    'contextual_help_messages/person_index.pt', 1)],
+
+	(Idea, 'to work', 'index'): [
+	   (None, 'novaideo:views/templates/panels/'
+	   	                    'contextual_help_messages/idea_to_work.pt', 1)],
+
+	(Idea, 'archived', 'index'): [
+	   (None, 'novaideo:views/templates/panels/'
+	   	                    'contextual_help_messages/idea_archived.pt', 1)],
+
+	(Idea, 'submited', 'index'): [
+	   (idea_submited_owner, 'novaideo:views/templates/panels/'
+	   	                    'contextual_help_messages/idea_submited_owner.pt', 1),
+	   (idea_submited_moderator, 'novaideo:views/templates/panels/'
+	   	                'contextual_help_messages/idea_submited_moderator.pt', 1)],
+
+	(Idea, 'published', 'index'): [
+	   (None, 'novaideo:views/templates/panels/'
+	   	                    'contextual_help_messages/idea_published.pt', 1)],
+
+	(Idea, 'any', 'duplicateidea'): [
+	   (None, 'novaideo:views/templates/panels/'
+	   	                    'contextual_help_messages/duplicate_idea.pt', 1)],
+
+	(Idea, 'any', 'publishasproposal'): [
+	   (None, 'novaideo:views/templates/panels/'
+	   	                    'contextual_help_messages/transform_idea.pt', 1)],
+
+}
+
--- novaideo/views/panels.py
+++ novaideo/views/panels.py
@@ -463,3 +463,39 @@ class Deadline_panel(object):
                 'current_date': current_date,
                 'previous_deadline': previous_deadline,
                 'condition': True}
+
+from novaideo.contextual_help_messages import CONTEXTUAL_HELP_MESSAGES
+
+@panel_config(
+    name='contextual_help',
+    context = Entity ,
+    renderer='templates/panels/contextual_help.pt'
+    )
+class ContextualHelp(object):
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def __call__(self):
+        if not self.request.cookies.get('contextual_help', True):
+            return {'condition': False}
+
+        user = get_current()
+        messages = [CONTEXTUAL_HELP_MESSAGES.get((self.context.__class__, 
+                                                  s, self.request.view_name), 
+                                                 None) \
+                    for s in self.context.state]
+        messages.append(CONTEXTUAL_HELP_MESSAGES.get(
+                        (self.context.__class__, 'any', self.request.view_name),
+                        None))
+        messages = [m for m in messages if m is not None]
+        messages = [item for sublist in messages for item in sublist]
+        messages = sorted(messages, key=lambda m: m[2])
+        messages = [ renderers.render(m[1],
+                      {'context':self.context,
+                       'user': user},
+                      self.request) for m in messages \
+                    if m[0] is None or m[0](self.context, user)]
+        return {'messages': messages,
+                'condition': True}
