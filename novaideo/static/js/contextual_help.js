0b64c42a5a6c6edb17382f3c85da93ab6da0426e
amen souissi <amensouissi@ecreall.com>
2015-03-23 17:26:32 +0100

    Fix dynamic contextual help

--- novaideo/static/js/contextual_help.js
+++ novaideo/static/js/contextual_help.js
@@ -5,31 +5,43 @@ function init_contextual_help(){
 	for(var i=0; i<helps.length; i++){
 		var help = $(helps[i]);
         help.data('top', help.offset().top);
-	}
+        if($('.principal-help').length == 0){  
+            help.addClass('hide-bloc');
+        }
+    }
 }
 
 $(document).ready(function(){
 	init_contextual_help();
 
-    $('a.panel-title').on('click', function(){
+    $('div.panel-collapse').on('shown.bs.collapse', function(){
+        var help = $('.'+ $(this).data('help'));
+        if(help.length>0){
+            var helps = $('.contextual-help-item');
+            var help_parent = $(help.parents('.contextual-help.alert').first());
+            var position = $(this).offset().top;
+            helps.addClass('hide-bloc');
+            help_parent.removeClass('hide-bloc');
+            help_parent.offset({top: position});
+            help.removeClass('hide-bloc');
+	        
+        }
+    });
+
+    $('div.panel-collapse').on('hide.bs.collapse', function(){
         var help = $('.'+ $(this).data('help'));
         var principal_help = $('.principal-help');
         if(help.length>0){
             var helps = $('.contextual-help-item');
             var help_parent = $(help.parents('.contextual-help.alert').first());
-            var position = $(this).offset().top;
-	        if($(this).hasClass('collapsed')){
-                helps.addClass('hide-bloc');
-                help_parent.offset({top: position});
-                help.removeClass('hide-bloc');
-
-	        }else{
-                helps.addClass('hide-bloc');
-                if(principal_help.length>0){
-                  help_parent.offset({top: parseInt(help_parent.data('top'))});
-                  principal_help.removeClass('hide-bloc')
-                }
-	        }
+            helps.addClass('hide-bloc');
+            if(principal_help.length>0){
+              help_parent.offset({top: parseInt(help_parent.data('top'))});
+              principal_help.removeClass('hide-bloc')
+            }else{
+              help_parent.addClass('hide-bloc');
+            }
+            
         }
     })
 })
