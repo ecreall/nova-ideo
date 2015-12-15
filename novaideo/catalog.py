aab6b9ded16d27ced111a19a71538e860b979e1a
Amen Souissi <amensouissi@ecreall.com>
2015-12-15 16:27:28 +0100

    fix index

--- novaideo/catalog.py
+++ novaideo/catalog.py
@@ -276,7 +276,10 @@ class SearchableObject(Adapter):
     def object_authors(self):
         author = getattr(self.context, 'author', None)
         if author:
-            return [get_oid(author)]
+            try:
+                return [get_oid(author)]
+            except:
+                return []
 
         return []
 
