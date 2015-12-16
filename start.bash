2a8fecd1cc4a89e875b9d6f45ca0c90df1befe26
Vincent Fretin <vincent.fretin@gmail.com>
2015-12-16 14:13:56 +0100

    create var/tmp_uploads var/tmp directories

--- start.bash
+++ start.bash
@@ -1,10 +1,15 @@
 #!/bin/bash
 sed -i 's@/tmp/build@/app@' develop-eggs/* bin/*
-MAIL_HOST="${MAIL_PORT_25_TCP_ADDR:-localhost}"
-MAIL_PORT="${MAIL_PORT_25_TCP_PORT:-25}"
+#MAIL_HOST="${MAIL_PORT_25_TCP_ADDR:-localhost}"
+#MAIL_PORT="${MAIL_PORT_25_TCP_PORT:-25}"
+# use 'mail' hostname which is updated in /etc/hosts when postfix container restart, but not the environment variables
+MAIL_HOST="${POSTFIX_HOST:-mail}"
+MAIL_PORT="${POSTFIX_PORT:-25}"
 MAIL_DEFAULT_SENDER="${MAIL_DEFAULT_SENDER:-site@example.com}"
 SECRET="${SECRET:-dreevTatUk9}"
 APPLICATION_URL="${APPLICATION_URL:-applicationurl}"
+TIMEOUT="${TIMEOUT:-30}"
+export TMPDIR="/app/var/tmp"
 sed -i \
     -e "s|MAIL_HOST|$MAIL_HOST|" \
     -e "s|MAIL_PORT|$MAIL_PORT|" \
@@ -12,6 +17,6 @@ sed -i \
     -e "s|SECRET|$SECRET|" \
     -e "s|APPLICATION_URL|$APPLICATION_URL|" \
     production-heroku.ini
-mkdir -p var/filestorage var/blobstorage
-chmod 700 var/filestorage var/blobstorage
+mkdir -p var/log var/filestorage var/blobstorage var/tmp_uploads var/tmp
+chmod 700 var/log var/filestorage var/blobstorage var/tmp_uploads var/tmp
 exec bin/gunicorn --forwarded-allow-ips="172.17.42.1" --access-logfile - --paste production-heroku.ini
