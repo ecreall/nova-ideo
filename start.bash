e7290ac9497ebcc4b0b03fc09604d51359e0c0c4
Vincent Fretin <vincent.fretin@gmail.com>
2016-01-08 17:52:40 +0100

    use WORKERS variable to be able to bootstrap the application
    Conflicts:
    	production-heroku.ini

--- start.bash
+++ start.bash
@@ -9,6 +9,7 @@ MAIL_DEFAULT_SENDER="${MAIL_DEFAULT_SENDER:-site@example.com}"
 SECRET="${SECRET:-dreevTatUk9}"
 APPLICATION_URL="${APPLICATION_URL:-applicationurl}"
 TIMEOUT="${TIMEOUT:-30}"
+WORKERS="${WORKERS:-1}"
 export TMPDIR="/app/var/tmp"
 sed -i \
     -e "s|MAIL_HOST|$MAIL_HOST|" \
@@ -16,6 +17,7 @@ sed -i \
     -e "s|MAIL_DEFAULT_SENDER|$MAIL_DEFAULT_SENDER|" \
     -e "s|SECRET|$SECRET|" \
     -e "s|APPLICATION_URL|$APPLICATION_URL|" \
+    -e "s|WORKERS|$WORKERS|" \
     production-heroku.ini
 mkdir -p var/log var/filestorage var/blobstorage var/tmp_uploads var/tmp
 chmod 700 var/log var/filestorage var/blobstorage var/tmp_uploads var/tmp
