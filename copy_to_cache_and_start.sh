ed47e7c28174b875d84cb2f233adda6399bbc3eb
Vincent Fretin <vincent.fretin@gmail.com>
2016-01-08 18:18:56 +0100

    remove chameleon/tests/ to fix permission issues

--- copy_to_cache_and_start.sh
+++ copy_to_cache_and_start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 rm -rf /app/cache/eggs
 rm -rf /app/cache/src
+# tests directory have permission issue, I don't understand why
+rm -rf /app/eggs/Chameleon-*.egg/chameleon/tests/
 cp -rf /app/eggs /app/cache/
 cp -rf /app/src /app/cache/
 exec /start
