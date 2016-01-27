ca6e5191aa68e743eb60be6f429db2575a809377
Amen Souissi <amensouissi@ecreall.com>
2016-01-27 15:55:15 +0100

    fix http error views

--- novaideo/views/http_views/__init__.py
+++ novaideo/views/http_views/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf8 -*-
-# Copyright (c) 2014 by Ecreall under licence AGPL terms 
-# avalaible on http://www.gnu.org/licenses/agpl.html 
+# Copyright (c) 2014 by Ecreall under licence AGPL terms
+# avalaible on http://www.gnu.org/licenses/agpl.html
 
 # licence: AGPL
 # author: Amen Souissi
@@ -8,16 +8,50 @@
 from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPNotFound, HTTPInternalServerError
 
+from pontus.view import BasicView
 
-@view_config(context=HTTPNotFound, 
-	         renderer='novaideo:views/http_views/templates/404.pt')
-def not_found(self, request):
-    request.response.status = 404
-    return {'request': request}
+from novaideo import _
 
 
-@view_config(context=HTTPInternalServerError, 
-	         renderer='novaideo:views/http_views/templates/500.pt')
-def internal_server_error(self, request):
-    request.response.status = 500
-    return {'request': request}
+@view_config(
+    context=HTTPNotFound,
+    renderer='pontus:templates/views_templates/grid.pt',
+    )
+class NotFoundView(BasicView):
+    title = _('Document not found!')
+    name = 'notfound'
+    template = 'novaideo:views/http_views/templates/404.pt'
+    css_class = 'simple-bloc'
+    container_css_class = 'home'
+    wrapper_template = 'novaideo:views/http_views/templates/simple_wrapper.pt'
+
+    def update(self):
+        self.title = self.request.localizer.translate(self.title)
+        result = {}
+        body = self.content(args={}, template=self.template)['body']
+        item = self.adapt_item(body, self.viewid)
+        result['coordinates'] = {self.coordinates: [item]}
+        self.request.response.status = 404
+        return result
+
+
+@view_config(
+    context=HTTPInternalServerError,
+    renderer='pontus:templates/views_templates/grid.pt',
+    )
+class InternalServerError(BasicView):
+    title = _('An error has occurred!')
+    name = 'internalservererror'
+    template = 'novaideo:views/http_views/templates/500.pt'
+    css_class = 'simple-bloc'
+    container_css_class = 'home'
+    wrapper_template = 'novaideo:views/http_views/templates/simple_wrapper.pt'
+
+    def update(self):
+        self.title = self.request.localizer.translate(self.title)
+        result = {}
+        body = self.content(args={}, template=self.template)['body']
+        item = self.adapt_item(body, self.viewid)
+        result['coordinates'] = {self.coordinates: [item]}
+        self.request.response.status = 500
+        return result
