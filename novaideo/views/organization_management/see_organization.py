b8f88c442b2e316e81a441221c259a1d7c5274be
Amen Souissi <amensouissi@ecreall.com>
2016-08-20 13:11:31 +0200

    improve organization views

--- novaideo/views/organization_management/see_organization.py
+++ novaideo/views/organization_management/see_organization.py
@@ -4,8 +4,12 @@
 # licence: AGPL
 # author: Amen Souissi
 
+import datetime
+import pytz
 from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPFound
+
+from substanced.util import Batch
 
 from dace.util import getSite
 from dace.processinstance.core import DEFAULTMAPPING_ACTIONS_VIEWS
@@ -14,11 +18,62 @@ from pontus.view import BasicView
 from novaideo.content.processes.organization_management.behaviors import (
     SeeOrganization)
 from novaideo.content.organization import Organization
-from novaideo.utilities.util import generate_navbars, ObjectRemovedException
+from novaideo.utilities.util import (
+    generate_navbars, ObjectRemovedException,
+    render_listing_objs)
+from novaideo.core import BATCH_DEFAULT_SIZE
+from novaideo import _
+
+
+CONTENTS_MESSAGES = {
+    '0': _(u"""no member"""),
+    '1': _(u"""one member"""),
+    '*': _(u"""${nember} members""")
+}
+
+
+class MembersView(BasicView):
+    template = 'novaideo:views/novaideo_view_manager/templates/home.pt'
+    wrapper_template = 'novaideo:views/templates/simple_wrapper.pt'
+    empty_message = _("No members")
+    empty_icon = 'glyphicon glyphicon-user'
+
+    def update(self):
+        user = self.context
+        objects = self.context.members
+        now = datetime.datetime.now(tz=pytz.UTC)
+        objects = sorted(
+            objects,
+            key=lambda e: getattr(e, 'modified_at', now),
+            reverse=True)
+        url = self.request.resource_url(
+            self.context, '@@seeorganization')
+        batch = Batch(objects,
+                      self.request,
+                      url=url,
+                      default_size=BATCH_DEFAULT_SIZE)
+        batch.target = "#results-members"
+        result_body, result = render_listing_objs(
+            self.request, batch, user)
+        len_members = batch.seqlen
+        index = str(len_members)
+        if len_members > 1:
+            index = '*'
+
+        self.title = _(CONTENTS_MESSAGES[index],
+                       mapping={'nember': len_members})
+        values = {'bodies': result_body,
+                  'batch': batch,
+                  'empty_message': self.empty_message,
+                  'empty_icon': self.empty_icon}
+        body = self.content(args=values, template=self.template)['body']
+        item = self.adapt_item(body, self.viewid)
+        result['coordinates'] = {self.coordinates: [item]}
+        return result
 
 
 @view_config(
-    name='',
+    name='seeorganization',
     context=Organization,
     renderer='pontus:templates/views_templates/grid.pt',
     )
@@ -36,9 +91,14 @@ class SeeOrganizationView(BasicView):
         except ObjectRemovedException:
             return HTTPFound(self.request.resource_url(getSite(), ''))
 
+        members_instance = MembersView(self.context, self.request)
+        members_result = members_instance.update()
+        members_body = members_result['coordinates'][MembersView.coordinates][0]['body']
         result = {}
         values = {
             'object': self.context,
+            'members_body': members_body,
+            'members_len': members_instance.title,
             'navbar_body': navbars['navbar_body'],
             'actions_bodies': navbars['body_actions'],
             'footer_body': navbars['footer_body']
