bb7551156bd6acc5522858fe3f5153938198e7c6
Amen Souissi <amensouissi@ecreall.com>
2016-09-20 09:42:58 +0200

    fix obj nb sync

--- novaideo/static/js/filter.js
+++ novaideo/static/js/filter.js
@@ -42,16 +42,14 @@ function filter(){
         }
         if(data['body']){
             var result_body = $('<div>'+data['body']+'</div>')
+            var source_tab_id = $(target.parents('.tab-pane').first()).attr('id')
             // update tab title
-            var tabs = $('.pontus-main ul.nav.nav-tabs > li > a')
-            var new_tabs = tabs.each(function(){
-              var tab = result_body.find('ul.nav.nav-tabs > li > a[href="'+$(this).attr('href')+'"]')
-              $(this).html(tab.html())
-            })
+            var source_tab = $('.pontus-main ul.nav.nav-tabs > li > a[href="#'+source_tab_id+'"]')
+            var tab = result_body.find('ul.nav.nav-tabs > li > a[href="#'+source_tab_id+'"]').first()
+            source_tab.html(tab.html())
             // update view title
             var new_title =  $(result_body.find('.filter-btn').first()).data('filter_message');
             target_title.html("<div class=\"panel-title\"><h4>"+new_title+"</h4></div>");
-            
             // update results
             target.html(result_body.find('#'+id).html());
            try {
