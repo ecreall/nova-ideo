3daf8e83b1b018ca2d4ff73734e8e5e65a8ad634
Amen Souissi <amensouissi@ecreall.com>
2017-03-02 20:53:08 +0100

    fix amendment message

--- novaideo/content/processes/work_mode_processes/amendment_work_mode_process/definition.py
+++ novaideo/content/processes/work_mode_processes/amendment_work_mode_process/definition.py
@@ -85,6 +85,10 @@ class SubProcess(OriginSubProcess):
             if vote_processes:
                 close_votes(None, request, vote_processes)
 
+        ballots = getattr(process, 'ballots', [])
+        for ballot in ballots:
+            ballot.finish_ballot()
+
         super(SubProcess, self).stop()
 
 
@@ -140,7 +144,7 @@ class SubProcessDefinitionAmendments(OriginSubProcessDefinition):
             ballot = Ballot('MajorityJudgment', electors,
                             group, AMENDMENTS_VOTE_DEFAULT_DURATION,
                             group=VOTE_AMENDMENT_GROUP,
-                            subjects=[proposal])
+                            subjects=group)
             working_group.addtoproperty('ballots', ballot)
             ballot.report.description = VOTE_AMENDMENTS_MESSAGE
             ballot.title = _('Vote on amendments (group ${nbi})',
