b2e891fc74bac04303b6c5a3a0343b91ee5d735f
Vincent Fretin <vincent.fretin@gmail.com>
2017-04-08 14:30:04 +0200

    can now use pdb with run.sh

--- start_all.bash
+++ start_all.bash
@@ -3,6 +3,6 @@ inifile=$1
 TIMEOUT=$2
 # 127.0.0.1 is when varnish->gunicorn in the same container
 exec gosu u1000 ./codep \
-    './bin/runzeo -C etc/zeo.conf' \
     "./bin/gunicorn --forwarded-allow-ips=127.0.0.1 --access-logfile - --paste $inifile -t $TIMEOUT" \
+    './bin/runzeo -C etc/zeo.conf' \
     "./start_system.bash $inifile"
