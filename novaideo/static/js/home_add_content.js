23ab9d91b7ede593f52a557f2dc82faf2db22dd4
Amen Souissi <amensouissi@ecreall.com>
2017-04-08 21:43:35 +0200

    fix attr name

--- novaideo/static/js/home_add_content.js
+++ novaideo/static/js/home_add_content.js
@@ -181,7 +181,7 @@ $(document).on('submit','.home-add-content .home-add-question form', function( e
        return
     }
     var parent = $($this.parents('.home-form-container').first());
-    var title = $this.find('input[name="question"]').val();
+    var title = $this.find('input[name="title"]').val();
     var keywords = $($this.find('select[name="keywords"]')).val();
     if(title=='' || !keywords || keywords.length == 0){
       var form_groups = $this.find('.form-group')
@@ -195,7 +195,7 @@ $(document).on('submit','.home-add-content .home-add-question form', function( e
       })
       if (title=='')
       {
-        form_group = $this.find('input[name="question"]').parents('.form-group').first()
+        form_group = $this.find('input[name="title"]').parents('.form-group').first()
         form_group.addClass('has-error')
         form_group.append($(error_help))
       }
@@ -237,7 +237,7 @@ $(document).on('submit','.home-add-content .home-add-question form', function( e
             result_container.find('.result-empty-message').first().remove()
             $(data.new_obj_body).hide().prependTo(result_container).fadeIn(1500)
           }
-          $this.find('input[name="question"]').val('');
+          $this.find('input[name="title"]').val('');
           $this.find('textarea[name="text"]').val('');
           $this.find('.deform-close-button').click()
           buttons.removeClass('disabled');
