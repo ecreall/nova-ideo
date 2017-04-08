379b763bc6ec75d9614850caf35c3ae968025814
Vincent Fretin <vincent.fretin@gmail.com>
2017-04-08 14:36:45 +0200

    add latestContentCreatedDate to stats json view

--- novaideo/views/stats.py
+++ novaideo/views/stats.py
@@ -14,14 +14,27 @@ from novaideo.content.interface import (
 def site_data(context, request):
     counts = {}
     dace_catalog = find_catalog('dace')
+    novaideo_catalog = find_catalog('novaideo')
+    created_at_index = novaideo_catalog['created_at']
     states_index = dace_catalog['object_states']
     object_provides_index = dace_catalog['object_provides']
+
+    latest_content_created_date = object_provides_index.any(
+        (IPerson.__identifier__,
+         Iidea.__identifier__,
+         IQuestion.__identifier__,
+         IProposal.__identifier__,
+         IChallenge.__identifier__)).execute(
+        ).sort(created_at_index, reverse=True).first().created_at
+
     query = object_provides_index.any((IPerson.__identifier__,)) & \
         states_index.notany(['deactivated'])
     counts['members'] = len(query.execute())
+
     query = object_provides_index.any((Iidea.__identifier__,)) & \
         states_index.any(['published'])
     counts['ideas'] = len(query.execute())
+
     counts['questions'] = 0
     if 'question' in request.content_to_manage:
         query = object_provides_index.any((IQuestion.__identifier__,)) & \
@@ -46,6 +59,7 @@ def site_data(context, request):
     logo = getattr(root, 'picture', None)
     result["logoUrl"] = logo.url if logo else \
         request.static_url('novaideo:static/images/novaideo_logo.png')
+    result["latestContentCreatedDate"] = latest_content_created_date.strftime('%Y-%m-%d')
     result["creationDate"] = root.created_at.strftime('%Y-%m-%d')
     result["title"] = root.title
     result["url"] = request.resource_url(root, '')
