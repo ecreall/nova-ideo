6588dd499885790b47d5f0cc43e5f539c4403573
Vincent Fretin <vincent.fretin@gmail.com>
2017-04-21 12:15:01 +0200

    point worker-tmp-dir to a tmpfs

--- start_all.bash
+++ start_all.bash
@@ -2,7 +2,11 @@
 inifile=$1
 TIMEOUT=$2
 # 127.0.0.1 is when varnish->gunicorn in the same container
+# /dev is a tmpfs in a docker container, use that for worker-tmp-dir, see
+# http://docs.gunicorn.org/en/stable/faq.html#how-do-i-avoid-gunicorn-excessively-blocking-in-os-fchmod
+mkdir /dev/tmp
+chown u1000:u1000 /dev/tmp
 exec gosu u1000 ./codep \
-    "./bin/gunicorn --forwarded-allow-ips=127.0.0.1 --access-logfile - --paste $inifile -t $TIMEOUT" \
+    "./bin/gunicorn --forwarded-allow-ips=127.0.0.1 --access-logfile - --paste $inifile -t $TIMEOUT --worker-tmp-dir /dev/tmp" \
     './bin/runzeo -C etc/zeo.conf' \
     "./start_system.bash $inifile"
--- start_system.bash
+++ start_system.bash
@@ -5,4 +5,4 @@ sed -e 's@dace.wosystem@dace.wosystem dace.system@' \
     -e 's@^port = .*@port = 5002@' \
     -e 's@^host = .*@host = 127.0.0.1@' \
     -e 's@^workers =.*@workers = 1@' $1 > production-system.ini
-exec ./bin/gunicorn --access-logfile - --paste production-system.ini -t 36000
+exec ./bin/gunicorn --access-logfile - --paste production-system.ini -t 36000 --worker-tmp-dir /dev/tmp
