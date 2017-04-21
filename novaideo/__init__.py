52f89fe6b36fb1ee7eef5a236b406160dada314e
Vincent Fretin <vincent.fretin@gmail.com>
2017-04-21 13:53:37 +0200

    Revert "evolve step to reset default locale to fr"
    This reverts commit 813b5d0f2d6cde885c4954971db8c8d35d838865.

--- novaideo/__init__.py
+++ novaideo/__init__.py
@@ -759,10 +759,6 @@ def evolve_mails_languages(root, registry):
     log.info('Emails evolved.')
 
 
-def evolve_reset_default_locale_to_fr(root, registry):
-    root.locale = 'fr'
-
-
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
@@ -809,7 +805,6 @@ def main(global_config, **settings):
     config.add_evolution_step(evolve_related_correlation)
     config.add_evolution_step(publish_organizations)
     config.add_evolution_step(evolve_mails_languages)
-    config.add_evolution_step(evolve_reset_default_locale_to_fr)
     config.add_translation_dirs('novaideo:locale/')
     config.add_translation_dirs('pontus:locale/')
     config.add_translation_dirs('dace:locale/')
