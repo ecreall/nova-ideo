11b2afea7cd97beb1d114f2ea7c7d3a8607ea015
Vincent Fretin <vincent.fretin@gmail.com>
2017-05-03 12:41:42 +0200

    fix typo avalaible -> available

--- novaideo/views/idea_management/associate.py
+++ novaideo/views/idea_management/associate.py
@@ -1,6 +1,6 @@
 # -*- coding: utf8 -*-
 # Copyright (c) 2014 by Ecreall under licence AGPL terms 
-# avalaible on http://www.gnu.org/licenses/agpl.html 
+# available on http://www.gnu.org/licenses/agpl.html
 
 # licence: AGPL
 # author: Amen Souissi
