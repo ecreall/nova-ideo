e4e81bd79969cf67734cbc97a8ae827670ef2f5c
Amen Souissi <amensouissi@ecreall.com>
2017-05-03 10:24:27 +0200

    add idea management tests

--- novaideo/testing.py
+++ novaideo/testing.py
@@ -4,6 +4,7 @@
 # licence: AGPL
 # author: Amen Souissi
 
+import pytz
 import unittest
 from pyramid import testing
 try:
@@ -15,6 +16,9 @@ except ImportError:
 from substanced.db import root_factory
 
 from dace.subscribers import stop_ioloop
+
+from novaideo import searchable_contents
+
 
 class BaseFunctionalTests(object):
 
@@ -49,14 +53,16 @@ class BaseFunctionalTests(object):
         self.request = request = testing.DummyRequest()
         self.config = testing.setUp(registry=app.registry, request=request)
         self.registry = self.config.registry
-        from .catalog import NovaideoIndexes, Text, Lexicon, Splitter, CaseNormalizer, StopWordRemover
+        from .catalog import (
+            NovaideoIndexes, Text,
+            Lexicon, Splitter,
+            CaseNormalizer, StopWordRemover)
         # lexicon is a persistent object, we need to be sure it's a fresh one
         # between tests
         NovaideoIndexes.relevant_data = Text(
             lexicon=Lexicon(Splitter(), CaseNormalizer(), StopWordRemover()))
         self.root = root_factory(request)
         request.root = self.root
-
 
     def tearDown(self):
         stop_ioloop()
@@ -70,6 +76,23 @@ class FunctionalTests(BaseFunctionalTests, unittest.TestCase):
 
     def setUp(self):
         super(FunctionalTests, self).setUp()
+
+    def default_request_setUp(self):
+        self.request.get_time_zone = pytz.timezone('Europe/Paris')
+        self.request.moderate_ideas = False
+        self.request.moderate_proposals = False
+        self.request.examine_ideas = False
+        self.request.examine_proposals = False
+        self.request.support_ideas = True
+        self.request.support_proposals = True
+        self.request.content_to_examine = []
+        self.request.content_to_support = ['idea', 'proposal']
+        self.request.accessible_to_anonymous = True
+        self.request.content_to_manage = [
+            'question', 'idea', 'proposal']
+        self.request.searchable_contents = searchable_contents(
+            self.request)
+        self.request.user = self.request.root['principals']['users']['admin']
 
 
 class RobotLayer(BaseFunctionalTests, Layer):
---
+++ novaideo/tests/test_idea_management.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2014 by Ecreall under licence AGPL terms
+# avalaible on http://www.gnu.org/licenses/agpl.html
+
+# licence: AGPL
+# author: Amen SOUISSI
+
+"""Tests for Idea management process
+"""
+
+from dace.util import getAllBusinessAction
+
+from novaideo.testing import FunctionalTests
+from novaideo.content.idea import Idea
+
+
+class TestIdeaManagement(FunctionalTests): #pylint: disable=R0904
+    """Test Vote integration"""
+
+    def setUp(self):
+        super(TestIdeaManagement, self).setUp()
+
+    def test_creat_idea_default_conf(self):
+        # SetUp the default Nova-Ideo configuration
+        self.default_request_setUp()
+        context = self.request.root
+        idea = Idea(
+            title="Idea title",
+            text="Idea text",
+            keywords=["keyword 1", "keyword 2"])
+        # Find the 'creat' action of the idea management process
+        actions = getAllBusinessAction(
+            context, self.request,
+            process_id='ideamanagement',
+            node_id='creat')
+        self.assertEqual(len(actions), 1)
+        creat_action = actions[0]
+        self.assertEqual(creat_action.node_id, 'creat')
+        self.assertEqual(creat_action.process_id, 'ideamanagement')
+        # Excute the action
+        creat_action.execute(
+            context, self.request, {'_object_data': idea})
+        ideas = context.ideas
+        self.assertEqual(len(ideas), 1)
+        idea_result = ideas[0]
+        self.assertIs(idea_result, idea)
+        self.assertIs(idea_result.author, self.request.user)
+        self.assertIn('to work', idea_result.state)
+        # Test if we have a single action
+        actions = getAllBusinessAction(
+            context, self.request,
+            process_id='ideamanagement',
+            node_id='creat')
+        self.assertEqual(len(actions), 1)
+
+    def test_creat_and_publish_idea_default_conf(self):
+        # SetUp the default Nova-Ideo configuration
+        self.default_request_setUp()
+        context = self.request.root
+        idea = Idea(
+            title="Idea title",
+            text="Idea text",
+            keywords=["keyword 1", "keyword 2"])
+        # Find the 'creat' action of the idea management process
+        actions = getAllBusinessAction(
+            context, self.request,
+            process_id='ideamanagement',
+            node_id='creatandpublish')
+        self.assertEqual(len(actions), 1)
+        creat_action = actions[0]
+        self.assertEqual(creat_action.node_id, 'creatandpublish')
+        self.assertEqual(creat_action.process_id, 'ideamanagement')
+        # Excute the action
+        creat_action.execute(
+            context, self.request, {'_object_data': idea})
+        ideas = context.ideas
+        self.assertEqual(len(ideas), 1)
+        idea_result = ideas[0]
+        self.assertIs(idea_result, idea)
+        self.assertIs(idea_result.author, self.request.user)
+        self.assertIn('published', idea_result.state)
+        # Test if we have a single action
+        actions = getAllBusinessAction(
+            context, self.request,
+            process_id='ideamanagement',
+            node_id='creatandpublish')
+        self.assertEqual(len(actions), 1)
