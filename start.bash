8701f21e8b7a7ec3e51dc8a94e3ff2337a6e2443
Amen Souissi <amensouissi@ecreall.com>
2017-05-29 10:51:59 +0200

    add environment variables for the yammer connector

--- start.bash
+++ start.bash
@@ -16,6 +16,8 @@ WORKERS="${WORKERS:-1}"
 DEFAULT_LOCALE="${DEFAULT_LOCALE:-en}"
 ENCRYPT_DATABASE="${ENCRYPT_DATABASE:-false}"
 KMI_SERVER="${KMI_SERVER:-https://encryptme.example.com}"
+YAMMER_CLIENT_ID="${YAMMER_CLIENT_ID:-}"
+YAMMER_CLIENT_SECRET="${YAMMER_CLIENT_SECRET:-}"
 export TMPDIR="/app/var/tmp"
 sed -i \
     -e "s|MAIL_HOST|$MAIL_HOST|" \
@@ -29,6 +31,8 @@ sed -i \
     -e "s|APPLICATION_URL|$APPLICATION_URL|" \
     -e "s|WORKERS|$WORKERS|" \
     -e "s|DEFAULT_LOCALE|$DEFAULT_LOCALE|" \
+    -e "s|YAMMER_CLIENT_ID|$YAMMER_CLIENT_ID|" \
+    -e "s|YAMMER_CLIENT_SECRET|$YAMMER_CLIENT_SECRET|" \
     production-heroku.ini
 sed \
     -e "s|ENCRYPT_DATABASE|$ENCRYPT_DATABASE|" \
