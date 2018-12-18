f9acbc2dce50b06bd7ace837e668209f10c3b47f
Amen Souissi <amensouissi@ecreall.com>
2018-12-18 09:38:23 +0100

    fix the output public path

--- novaideo/static-react/webpack.config.js
+++ novaideo/static-react/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     output: {
         path: path.join(__dirname, 'build'),
         filename: '[name].js',
-        publicPath: '/build/'
+        publicPath: '/novaideostatic-react/build/'
     },
     module: {
         rules: [
